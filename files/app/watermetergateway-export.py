dd2c8cd71e096bf47d52a084a7db944da88bc796
Sander Revenberg <sander@revenberg.info>
2022-08-26 19:38:06 +0200

    .

--- files/app/watermetergateway-export.py
+++ files/app/watermetergateway-export.py
@@ -24,20 +24,28 @@ class AppMetrics:
     Representation of Prometheus metrics and loop to fetch and transform
     application metrics into Prometheus metrics.
     """
+    _prometheus = {}
 
     def __init__(self, PROMETHEUS_PREFIX='', APIKEY='', WEATHER_COUNTRY='NL', WEATHER_LANGUAGE='NL', polling_interval_seconds=5):
 
         if PROMETHEUS_PREFIX != '':
             PROMETHEUS_PREFIX = PROMETHEUS_PREFIX + "_"
 
-        self.PROMETHEUS_PREFIX = PROMETHEUS_PREFIX
         self.IP = IP
         self.polling_interval_seconds = polling_interval_seconds
 
         # Prometheus metrics to collect
-        self.status = Info(PROMETHEUS_PREFIX + 'status', 'status')
-        self.wind_speed = Gauge(PROMETHEUS_PREFIX + 'wind_speed', 'wind_speed')
-        
+        self._prometheus['mac_address']                 = Info(self.PROMETHEUS_PREFIX + 'mac_address', 'mac_address model')
+        self._prometheus['gateway_model']               = Info(self.PROMETHEUS_PREFIX + 'gateway_model', 'gateway model')
+        self._prometheus['startup_time']                = Info(self.PROMETHEUS_PREFIX + 'startup_time startup_time')
+        self._prometheus['firmware_running']            = Info(self.PROMETHEUS_PREFIX + 'firmware_running', 'firmware_running')
+        self._prometheus['firmware_update_available']   = Info(self.PROMETHEUS_PREFIX + 'firmware_update_available', 'firmware_update_available')
+        self._prometheus['watermeter_value']            = Gauge(self.PROMETHEUS_PREFIX + 'watermeter_value', 'watermeter_value')
+        self._prometheus['watermeter_pulse_factor']     = Gauge(self.PROMETHEUS_PREFIX + 'watermeter_pulse_factor', 'watermeter_pulse_factor')
+        self._prometheus['watermeter_used_last_minute'] = Gauge(self.PROMETHEUS_PREFIX + 'watermeter_used_last_minute', 'watermeter_used_last_minute')
+        self._prometheus['watermeter_pulsecount']       = Gauge(self.PROMETHEUS_PREFIX + 'watermeter_pulsecount', 'watermeter_pulsecount')
+        self._prometheus['leak_detect']                 = Info(self.PROMETHEUS_PREFIX + 'leak_detect', 'leak_detect', states=['false', 'true'])
+
     def run_metrics_loop(self):
         """Metrics fetching loop"""
 
@@ -56,7 +64,10 @@ class AppMetrics:
             response.raise_for_status()
             LOG.info("The request was a success!")
             LOG.info(response.text())
-            print(response.json())            
+            print(response.json())
+            for v in response.json():
+                print(v)
+
         except requests.exceptions.HTTPError as error:
             LOG.error(error)
 
